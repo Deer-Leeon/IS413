067a59c363c0bc003a000cb902d6feb9e9c6e05c
Deer-Leeon <lj.buchmiller@gmail.com>
2025-02-20 20:22:02 -0700

    first changes

--- Mission07_Buchmiller/Mission06_Buchmiller/Models/Movie.cs
+++ Mission07_Buchmiller/Mission06_Buchmiller/Models/Movie.cs
@@ -4,19 +4,18 @@ namespace Mission06_Buchmiller.Models
 {
     public class Movie
     {
-        public int Id { get; set; }
+        public int MovieId { get; set; }
+        
+        public int CategoryId { get; set; }
 
         [Required]
         public string Title { get; set; }
 
         [Required]
         public int Year { get; set; }
-
+        
         [Required]
-        public string Category { get; set; }
-
-        [Required]
-        public string Director { get; set; }
+        public string? Director { get; set; }
 
         [Required]
         public string Rating { get; set; }
@@ -24,6 +23,9 @@ namespace Mission06_Buchmiller.Models
         public bool Edited { get; set; } // Now non-nullable
 
         public string? LentTo { get; set; } // Nullable is fine
+        
+        [Required]
+        public bool? CopiedToPlex { get; set; }
 
         [MaxLength(25)]
         public string? Notes { get; set; } // Nullable is fine
